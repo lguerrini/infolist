4c4a083a2dadf1c10ec6d9ea32a34cccf867c9c6
luca lorenzo guerrin <luca lorenzo guerrin@LAPTOP-AT03J01J.HP-gugudesign2>
2019-11-01 13:05:21 +0100

    Revert "Revert "nuovi package""
    This reverts commit 0661c990ed6a154e930aff956f57227debd79dd9.

---
+++ src/oggetti/Persona.java
@@ -0,0 +1,51 @@
+package oggetti;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author luca lorenzo guerrin
+ */
+public class Persona {
+    int id ;
+     String  firstname;
+     String lastname;
+     int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+     
+}
